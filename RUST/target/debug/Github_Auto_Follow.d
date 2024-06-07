04ff65c587bf4d0d26f9dc7e740c33f5de024115
dongwookkim3 <dongwook.3.kim@gmail.com>
2024-06-07 18:26:45 +0900

    TEST : RUST PUSH TEST

---
+++ RUST/target/debug/Github_Auto_Follow.d
@@ -0,0 +1,1 @@
+/home/dongwook/GIthub_Auto_Follow/RUST/target/debug/Github_Auto_Follow: /home/dongwook/GIthub_Auto_Follow/RUST/src/main.rs
---
+++ RUST/target/debug/deps/Github_Auto_Follow-a72935086e9ca572.d
@@ -0,0 +1,5 @@
+/home/dongwook/GIthub_Auto_Follow/RUST/target/debug/deps/Github_Auto_Follow-a72935086e9ca572: src/main.rs
+
+/home/dongwook/GIthub_Auto_Follow/RUST/target/debug/deps/Github_Auto_Follow-a72935086e9ca572.d: src/main.rs
+
+src/main.rs:
